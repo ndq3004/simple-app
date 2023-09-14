3f435173b0ad2970c590a486eff6fef627ecbd17
ndq30041998 <34181008+ndq3004@users.noreply.github.com>
2023-09-14 20:04:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World 123');
+  res.send('Hello World everyone');
 });
  
 app.listen(PORT, HOST, () => {
